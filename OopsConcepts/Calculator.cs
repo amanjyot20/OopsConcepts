2909fad7611c2edaf68201c4b06f6bc36f8a8d32
Aman Jyoti <amanjyot20@gmail.com>
2025-06-20 07:18:11 +0530

    Add project files.

---
+++ OopsConcepts/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts;
+public class Calculator
+{
+
+    public void Sum()
+    {
+        Console.WriteLine("Sum of 2 numbers");
+    }
+}
+
---
+++ OopsConcepts/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OopsConcepts;
+
+Calculator calculator = new Calculator();
+
+calculator.Sum(); 
+Console.WriteLine("Hello, World!");
